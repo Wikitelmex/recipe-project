5bf07bbb70ce1d5ee84fda97e913feb59ca65868
Alex Castillo <wikitelmex@gmail.com>
2022-02-08 09:01:54 -0700

    # Recipe model and Migration - create recipe model with validators and relationships - add user model relationship to recipes - create migration - implement migration to database and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_193511) do
+ActiveRecord::Schema.define(version: 2022_02_08_155113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.time "preparation_time"
+    t.time "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
