3aa7b05a368cc31c4148865b6d3c298b5958dd02
Alex Castillo <wikitelmex@gmail.com>
2022-02-09 08:18:54 -0700

    # ALERT direct changes pushed to develop
    - recipefood model
    - recipefood validations
    - recipefood relationships
    - recipefood migrations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
 
   belongs_to :user
+  has_many :recipe_foods
+  has_many :foods, through: :recipe_foods
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_211846) do
+ActiveRecord::Schema.define(version: 2022_02_09_151058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2022_02_08_211846) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.decimal "quantity", precision: 5, scale: 2
+    t.integer "recipe_id"
+    t.integer "food_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "recipes", force: :cascade do |t|
