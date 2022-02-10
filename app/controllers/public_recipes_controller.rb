eb57be5f8892cb221ec18be9e1ca1125afbaa92d
Alex Castillo <wikitelmex@gmail.com>
2022-02-10 14:04:59 -0700

    - public recipes improvements - logout feature - add info to home page - restriction to no owners in public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,13 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.where(public: true)
+    @recipes = Recipe.includes(:recipe_foods, :foods).where(public: true).map do |recipe|
+      {
+        id: recipe.id,
+        name: recipe.name,
+        user: recipe.user.name,
+        ingredients_amount: recipe.recipe_foods.count,
+        ingredients_price: recipe.foods.map(&:price).sum
+      }
+    end
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -23,9 +23,11 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    binding.break
+    sign_out(current_user)
+    redirect_to root_path
+  end
 
   # protected
 
